83080f798d707b116e1154faddaf8dd7bc29f488
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-15 18:38:57 +0530

    capitalise PORT

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Worlds!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Worlds!");
+});
 
-app.listen(process.env.port || port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening at ${port}`);
+});
